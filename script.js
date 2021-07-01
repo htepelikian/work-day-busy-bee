c5496af23abc76315d063fe9dde7aeaba28251ef
Hripsime Tepelikian <htepelikian@gmail.com>
2021-06-30 20:29:14 -0700

    added variables with date format and current time display, save to local storage

---
+++ script.js
@@ -0,0 +1,39 @@
+//----Variables with date format and current time display----//
+var date = moment().format("MMM Do YYYY");
+var currentTime = moment().format('H');
+console.log(currentTime);
+
+$("#currentDay").append(date);
+
+
+var timeOfday = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+updatetime();
+
+function updatetime() {
+  var currentTime = moment().format('H');
+  for(var i = 0; i < timeOfday.length; i++) {
+
+    if (parseInt(timeOfday[i]) > currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: ##77dd77");
+
+
+    }
+    else if (parseInt(timeOfday[i]) < currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #d3d3d3");
+
+    }
+    else if (parseInt(timeOfday[i]) == currentTime) {
+      $("#" + timeOfday[i]).attr("style", "background-color: #ff6961");
+    
+    }
+  }
+}
+
+//--Save input to local storage---//
+$(".rowBtn").on("click", function() {
+    var timeOfday = $(this).parent().attr("id");
+    var textContent = $("input").val().trim();
+
+    localStorage.setItem(timeOfday, textContent);
+    console.log(timeOfday, textContent);
+});
